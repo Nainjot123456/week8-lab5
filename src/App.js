4350fc897dc35072a62b970b4d3c2d0b56470e51
Nainjot Singh <singhnainjot6@gmail.com>
2023-12-04 12:52:22 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forms Lab</h1>
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
